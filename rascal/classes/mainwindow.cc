f0d622f5aac4b7194ab2750e0f847692df1dd45b
Simon Gornall <sgornall@apple.com>
2021-06-13 11:36:45 -0700

    Change the layout slightly

--- rascal/classes/mainwindow.cc
+++ rascal/classes/mainwindow.cc
@@ -48,12 +48,12 @@ void MainWindow::_createUI(void)
 	_waterfall	= new Waterfall(this);
 
 	QGridLayout *grid = new QGridLayout;
-	grid->addWidget(_graph, 0, 0, 8, 22);
-	grid->addWidget(_sky, 0, 22, 8, 16);
-	grid->addWidget(_limits, 8, 0, 1, 22);
-	grid->addWidget(_waterfall, 9, 0, 7, 22);
-	grid->addWidget(_events, 8, 22, 6, 16);
-	grid->addWidget(_vcr, 14, 22, 2, 16);
+	grid->addWidget(_graph, 0, 0, 16, 44);
+	grid->addWidget(_sky, 0, 44, 16, 32);
+	grid->addWidget(_limits, 16, 0, 1, 44);
+	grid->addWidget(_waterfall, 17, 0, 15, 44);
+	grid->addWidget(_events, 16, 44, 12, 32);
+	grid->addWidget(_vcr, 28, 44, 4, 32);
 
 	QWidget *top = findChild<QWidget *>("top");
 	top->setLayout(grid);
